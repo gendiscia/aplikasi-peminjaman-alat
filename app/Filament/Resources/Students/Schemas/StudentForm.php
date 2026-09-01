06a2b98df3407c34640ae5c5cbdf3bcd3c0b7e1b
gendis <gendis@gmail.com>
2026-09-01 13:52:32 +0700

    Initial commit

---
+++ app/Filament/Resources/Students/Schemas/StudentForm.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Filament\Resources\Students\Schemas;
+
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\Textarea;
+use Filament\Schemas\Schema;
+
+class StudentForm
+{
+    public static function configure(Schema $schema): Schema
+    {
+        return $schema
+            ->components([
+                TextInput::make('user_id')
+                    ->required()
+                    ->numeric(),
+                TextInput::make('classroom_id')
+                    ->required()
+                    ->numeric(),
+                TextInput::make('nisn')
+                    ->required(),
+                TextInput::make('phone_number')
+                    ->tel()
+                    ->required(),
+                Select::make('gender')
+                    ->options(['male' => 'Male', 'female' => 'Female'])
+                    ->required(),
+                Textarea::make('address')
+                    ->columnSpanFull(),
+                TextInput::make('profile_picture'),
+            ]);
+    }
+}
